e08386ea05bdb3ed843e28a0a7e140946263cd2e
Salts-Spray <114344822+Salts-Spray@users.noreply.github.com>
2023-09-26 13:08:08 +0530

    Update loginCre.py

--- loginCre.py
+++ loginCre.py
@@ -10,8 +10,8 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.get("https://www.zimyo.work/")
 
 # Variables
-logintxt = "9975871746"
-passtxt = "#Del120@#908;)"
+logintxt = "Enter Login ID"
+passtxt = "Enter Password"
 
 # Login with Credentials
 driver.find_element("xpath","/html/body/div/main/div/div[2]/div/div[2]/form/div/div[1]/div/input").send_keys(logintxt)
@@ -20,19 +20,6 @@ driver.find_element("xpath","/html/body/div/main/div/div[2]/div/div[2]/form/div/
 time.sleep(2)
 driver.find_element("xpath","/html/body/div/main/div/div[2]/div/div[2]/button[1]/span[1]").click()
 time.sleep(10)
-
-
-# Break Over
-# driver.find_element("xpath","/html/body/div/div/div/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div/div[2]/div[3]/button").click()
-# time.sleep(2)
-# test
-# def login():
-#     driver.find_element(By.NAME, 'username').send_keys(logintxt)
-#     wait(1)
-#     driver.find_element(By.ID, 'password').send_keys('qwerty')
-#     wait(1)
-#     driver.find_element(By.ID, 'login').click()
-#     wait(1)
 
 # Browser Sleep
 time.sleep(10)
